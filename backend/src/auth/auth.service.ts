2857a650195b727fbdaf29bd99a44b6b121c38e2
Cauã Vinicius <cauavinicius.contato@gmail.com>
2025-10-25 07:03:01 -0300

    fix: add missing relation creation

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -31,6 +31,9 @@ export class AuthService {
       data: {
         ...registerUserDto,
         password: hashedPassword,
+        profileData: {
+          create: {},
+        },
       },
       select: {
         id: true,
